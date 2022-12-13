10dd1fafe8d81584156036a88a8f2752cbc423f6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-12-14 01:55:33 +0530

    Create Hello.vb

---
+++ Hello.vb
@@ -0,0 +1,11 @@
+Imports System 'System is a Namespace  
+Module Hello_Program  ' This is like class in java
+  
+  Sub Main()  ' main method like Java 
+  
+    Console.WriteLine("Hello Sona")  
+  
+    'System.Console.WriteLine("Hello Sona")
+  End Sub  ' end of main method
+  
+End Module  
